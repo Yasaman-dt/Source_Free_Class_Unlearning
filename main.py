ae8fde03045f7488efa85a17094e072ee74a7cdd
Yasaman-dt <yasaman.dehghani74@gmail.com>
2026-06-11 11:06:37 -0400

    update

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import os
 import torch
 import numpy as np
 from generate_emb_samples_randomly import generate_emb_samples_balanced
-#from generate_emb_samples_randomly_conf import generate_emb_samples_balanced
 from create_embeddings_utils import get_model
 from torch.utils.data import TensorDataset, DataLoader
 from Unlearning_methods import calculate_accuracy
